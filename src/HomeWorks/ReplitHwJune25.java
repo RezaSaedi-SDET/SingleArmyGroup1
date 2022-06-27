cf28221bc8f8d6eda87ddbf6fe8eccbefa3ef390
MRezaSaedi <mreza.saedi@gmail.com>
2022-06-26 22:50:37 -0700

    06-26-2022 2nd commit

--- src/HomeWorks/ReplitHwJune25.java
+++ src/HomeWorks/ReplitHwJune25.java
@@ -2,6 +2,163 @@ package HomeWorks;
 
 public class ReplitHwJune25 {
 
+        /*
+        Create a static method with the following specification
+
+Return Type: String
+
+Method Name: mixString
+
+Parameters:
+
+a String called s1
+a String called s2
+Then inside method write logic that will return the strings combined, one letter at a time, starting with s1. See example below for an example.
+
+NOTE: s1 and s2 should be of equal lengths:
+
+s1 ==> "12345"
+s2 ==> "abcde"
+mixString(s1,s2) ==> "1a2b3c4d5e"
+         */
+    public static String mixString(String s1, String s2){
+      return s1+s2;
+    }
+
+    public static void main(String[] args) {
+        String m=mixString("12345","abcde");
+        System.out.println(m);
+    }
+
+        /*
+    For you to do:
+
+    Create two methods:
+
+    The first method should be a non-static method that will print out the following message:
+
+    "Programming is amazing."
+
+    The second method should be a static method that will print out the following message:
+
+    "Java is awesome."
+
+    Execute both methods
+
+    Expected Output:
+
+    Programming is amazing.
+    Java is awesome.
+     */
+
+//    void printInfo(){
+//        System.out.println("Programming is amazing.");
+//    }
+//
+//    public static void printInfo2(){
+//        System.out.println("Java is awesome.");
+//    }
+//
+//    public static void main(String[] args) {
+//        ReplitHwJune25 obj=new ReplitHwJune25();
+//        obj.printInfo();
+//        printInfo2();
+//    }
+
+    /*
+    For you to do:
+
+    Create a variable that will hold the count of all instances of the Main class
+
+    Create 3 Object of the class and print value of the count variable;
+
+    Expected Output:
+
+    3
+     */
+
+//    public static int num;
+//
+//
+//    public static void main(String[] args) {
+//        System.out.println(num=3);
+//    }
+
+    /*
+    For you to do:
+
+    Declare a static variable number that will hold an integer value:
+
+    Access number from the main method and assign value to it.
+
+    Create an Object of the class, access number in a nonstatic way and assing value of 200.
+
+    Print out number using class name and using instance
+
+    Expected Output:
+
+    200
+    200
+     */
+
+//    public static int num;
+//    int number(int num){
+//        return num;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(num=200);
+//        ReplitHwJune25 obj=new ReplitHwJune25();
+//        int m=obj.number(200);
+//        System.out.println(m);
+//
+//
+//    }
+
+    /*
+    Declare static variable in class level as below and assign its value:
+
+    String ss="Welcome To Syntax Technologies"
+
+    Access variable in the main method and print it using three ways you learned so far
+
+    Hint:
+
+    first way: By calling directly
+
+    Second way: By using the className
+
+    Third way: By creating the object of the class
+
+    Expected Output:
+
+    Welcome To Syntax Technologies
+    Welcome To Syntax Technologies
+    Welcome To Syntax Technologies
+     */
+
+
+//    static String ss="Welcome To Syntax Technologies";
+//
+//    static void displayInfo(){
+//        String ss="Welcome To Syntax Technologies";
+//        System.out.println(ss);
+//    }
+//    void displayInfo2(){
+//        String ss="Welcome To Syntax Technologies";
+//        System.out.println(ss);
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(ss);
+//        displayInfo();
+//        ReplitHwJune25 obj=new ReplitHwJune25();
+//        obj.displayInfo2();
+
+
+//    }
+
+
     /*
     For you to do:
 
